5f4a7d923c51ca2ba3567b7751563a5f3e8f6609
Yan Zhang <yanzhang0219@gmail.com>
2020-06-29 20:54:57 -0700

    reorganize fzf configs files: move them to ~/.config/fzf/

---
+++ .config/fzf/fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/yanzhang/gitrepos/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/yanzhang/gitrepos/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/yanzhang/gitrepos/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/yanzhang/gitrepos/fzf/shell/key-bindings.bash"

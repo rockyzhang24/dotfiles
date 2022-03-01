6cbc7e1eecda197a98d4e82adc3f63d0c4853eee
Rocky Zhang <yzhang.rocky@gmail.com>
2022-02-28 16:03:23 -0800

    nvim: change lualine separator to powerline angle

--- .config/nvim/lua/plugin_config/lualine.lua
+++ .config/nvim/lua/plugin_config/lualine.lua
@@ -9,8 +9,10 @@ require'lualine'.setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    -- component_separators = { left = '', right = ''},
+    -- section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {},
     always_divide_middle = true,
   },

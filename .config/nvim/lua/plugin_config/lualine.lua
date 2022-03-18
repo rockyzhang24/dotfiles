330128dd02a2ba1bd11c8dbda4aaaeb39c853ae1
Rocky Zhang <yzhang.rocky@gmail.com>
2022-03-18 12:42:01 -0700

    nvim: update lualine config

--- .config/nvim/lua/plugin_config/lualine.lua
+++ .config/nvim/lua/plugin_config/lualine.lua
@@ -5,6 +5,17 @@ local function gutenTagsProgress()
   return vim.fn['gutentags#statusline']('[', ']')
 end
 
+-- For mode, only show the first char (or first two chars to distinguish
+-- different VISUALs)
+local function simplifiedMode(str)
+  return str == "V-LINE" and "VL" or (str == "V-BLOCK" and "VB" or str:sub(1,1))
+end
+
+-- For location, show total lines
+local function customLocation(str)
+  return string.gsub(str, "%w+", "%1" .. "/%%L", 1)
+end
+
 require'lualine'.setup {
   options = {
     icons_enabled = true,
@@ -19,7 +30,12 @@ require'lualine'.setup {
   },
   sections = {
     -- Left
-    lualine_a = {'mode'},
+    lualine_a = {
+      {
+        'mode',
+        fmt = simplifiedMode,
+      },
+    },
     lualine_b = {
       'branch',
       {
@@ -40,18 +56,36 @@ require'lualine'.setup {
         symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
       }
     },
-    lualine_c = {'filename'},
+    lualine_c = {
+      {
+        'filename',
+        symbols = {
+          modified = '[+]',
+          readonly = '[]',
+          unnamed = '[No Name]',
+        },
+      },
+    },
 
     -- Right
     lualine_x = {
-      { gps.get_location, cond = gps.is_available },
+      {
+        gps.get_location,
+        cond = gps.is_available,
+        color = { fg = '#d3869b' },
+      },
       gutenTagsProgress,
       'encoding',
       'fileformat',
       'filetype'
     },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_y = {
+      {
+        'location',
+        fmt = customLocation,
+      },
+    },
+    lualine_z = {'progress'},
   },
   inactive_sections = {
     lualine_a = {},

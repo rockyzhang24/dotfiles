97508cfdd506aea73cd64b14de9c5963987269f4
Rocky Zhang <yzhang.rocky@gmail.com>
2023-05-05 00:12:48 -0700

    nvim: automatically toggle relative and absolute numbers

--- .config/nvim/lua/rockyz/autocmds.lua
+++ .config/nvim/lua/rockyz/autocmds.lua
@@ -1,5 +1,6 @@
 local fn = vim.fn
 local api = vim.api
+local cmd = vim.cmd
 
 -- Auto-create dir when saving a file, in case some intermediate directory does not exist
 api.nvim_create_augroup("auto_create_dir", {
@@ -15,4 +16,27 @@ api.nvim_create_autocmd({ "BufWritePre" }, {
       fn.mkdir(dir, 'p')
     end
   end
+})
+
+-- Automatically toggle the relative and absolute numbers
+-- Copy the code from https://github.com/sitiom/nvim-numbertoggle
+local augroup = api.nvim_create_augroup("numbertoggle", { clear = true })
+api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  pattern = "*",
+  group = augroup,
+  callback = function()
+    if vim.o.nu and api.nvim_get_mode().mode ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  pattern = "*",
+  group = augroup,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      cmd("redraw")
+    end
+  end,
 })

9093df11c4659fceb2f877a2fb9ecfaabcb4fdc1
Rocky Zhang <yzhang.rocky@gmail.com>
2024-02-04 00:57:17 -0800

    nvim: add keymap to preview markdown via Marked 2

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,4 +1,12 @@
--- Align the markdown table when typing |
-vim.keymap.set('i', '<Bar>', "<Bar><Esc>:lua require('rockyz.utils').md_table_bar_align()<CR>a", {
+local opts = {
+  buffer = true,
   silent = true,
-})
+}
+
+-- Align the markdown table when typing |
+vim.keymap.set('i', '<Bar>', "<Bar><Esc>:lua require('rockyz.utils').md_table_bar_align()<CR>a", opts)
+
+-- Open the current markdown via Marked 2 for preview
+vim.keymap.set('n', '<Leader>v', function()
+  vim.system({ 'open', '-a', 'Marked 2', vim.fn.bufname() }, {}, nil)
+end, opts)

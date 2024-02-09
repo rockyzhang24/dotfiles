67a9cd5d9da568669431860f26b6fc5e0d449bad
Rocky Zhang <yzhang.rocky@gmail.com>
2024-02-09 12:35:44 -0800

    nvim: add a command to open a scratch buffer

--- .config/nvim/lua/rockyz/commands.lua
+++ .config/nvim/lua/rockyz/commands.lua
@@ -32,3 +32,18 @@ end, {})
 vim.api.nvim_create_user_command('ProfileStop', function()
   require('plenary.profile').stop()
 end, {})
+
+-- Open a scratch buffer
+vim.api.nvim_create_user_command('Scratch', function()
+  vim.cmd('belowright 10new')
+  local buf = vim.api.nvim_get_current_buf()
+  for name, value in pairs {
+    filetype = 'scratch',
+    buftype = 'nofile',
+    bufhidden = 'hide',
+    swapfile = false,
+    modifiable = true,
+  } do
+  vim.api.nvim_set_option_value(name, value, { buf = buf })
+end
+end, { nargs = 0 })

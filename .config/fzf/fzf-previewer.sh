3fae589dd429353fabd4bc03a17831db1cbfd82c
Rocky Zhang <yzhang.rocky@gmail.com>
2024-03-15 14:50:10 -0700

    fzf: support image preview in kitty

--- .config/fzf/fzf-previewer.sh
+++ .config/fzf/fzf-previewer.sh
@@ -5,10 +5,19 @@ if [[ $(file -b "$1") == directory ]]; then
   exit
 fi
 
-mime=$(file --dereference --brief --mime "$1")
+mime=$(file --dereference --brief --mime-type "$1")
 
-if [[ $mime =~ =binary ]]; then
+if [[ $mime =~ \-binary ]]; then
   file "$1"
+  exit
+fi
+
+# In kitty (for both normal terminal and tmux), preview image via kitty image protocal. Introduced
+# since fzf 0.43.0.
+if [[ -n $KITTY_WINDOW_ID && $mime =~ image/ ]]; then
+  # --transfer-mode=memory is the fastest option but if you want fzf to be able to redraw the image
+  # on terminal resize or on 'change-preview-window', you need to use --transfer-mode=stream.
+  kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0 "$1" | sed '$d' | sed $'$s/$/\e[m/'
   exit
 fi
 

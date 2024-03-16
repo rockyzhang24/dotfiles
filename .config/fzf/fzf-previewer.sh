c4598ace43762a9825c772ccc4a6a77215c16691
Rocky Zhang <yzhang.rocky@gmail.com>
2024-03-15 20:10:48 -0700

    fzf: in image and video preview use file command as fallback

--- .config/fzf/fzf-previewer.sh
+++ .config/fzf/fzf-previewer.sh
@@ -12,18 +12,27 @@ if [[ $mime =~ \-binary ]]; then
   exit
 fi
 
-# In kitty (for both normal terminal and tmux), preview image via kitty image protocal. Introduced
-# since fzf 0.43.0.
-if [[ -n $KITTY_WINDOW_ID && $mime =~ image/ ]]; then
+# In kitty, preview image via kitty image protocal. Introduced since fzf 0.43.0. It works in the
+# normal terminal and tmux but not in Neovim's builtin term.
+# Use file command as the fallback. It outputs the file properties, separated by comma, in a single
+# lone line. In order to fit the width of the preview window, I replace each comma with a line
+# break, but leave the commas inside square brackets unchanged.
+if [[ -n $KITTY_WINDOW_ID && $mime =~ image/ && -z $NVIM ]]; then
   # --transfer-mode=memory is the fastest option but if you want fzf to be able to redraw the image
   # on terminal resize or on 'change-preview-window', you need to use --transfer-mode=stream.
   kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0 "$1" | sed '$d' | sed $'$s/$/\e[m/'
   exit
+else
+  file "$1" | gsed -r ':a; s/(\[[^][]*),([^][]*\])/\1TTEEMMPP\2/g; ta; s/, /\n/g; s/TTEEMMPP/,/g'
+  exit
 fi
 # Video can be previewed by previewing its thumbnail
-if [[ -n $KITTY_WINDOW_ID && $mime =~ video/|audio/ ]]; then
+if [[ -n $KITTY_WINDOW_ID && $mime =~ video/|audio/ && -z $NVIM ]]; then
   thumbnail=$($HOME/.config/lf/vidthumb "$1")
   kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0 "$thumbnail" | sed '$d' | sed $'$s/$/\e[m/'
+  exit
+else
+  file "$1" | gsed -r ':a; s/(\[[^][]*),([^][]*\])/\1TTEEMMPP\2/g; ta; s/, /\n/g; s/TTEEMMPP/,/g'
   exit
 fi
 

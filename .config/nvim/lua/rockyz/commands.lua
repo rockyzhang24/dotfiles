cabc60f16a9b30349a7df67e640528fe65dcfa33
Rocky Zhang <yzhang.rocky@gmail.com>
2024-04-22 16:24:28 -0700

    nvim: add commands to redirect :messages output

--- .config/nvim/lua/rockyz/commands.lua
+++ .config/nvim/lua/rockyz/commands.lua
@@ -1,3 +1,16 @@
+local function scratch_buf_init()
+  local buf = vim.api.nvim_get_current_buf()
+  for name, value in pairs({
+    filetype = 'scratch',
+    buftype = 'nofile',
+    bufhidden = 'hide',
+    swapfile = false,
+    modifiable = true,
+  }) do
+    vim.api.nvim_set_option_value(name, value, { buf = buf })
+  end
+end
+
 -- Close windows by giving window numbers, e.g., :CloseWin 1 2 3
 vim.api.nvim_create_user_command('CloseWin', function(opts)
   require('rockyz.utils').close_wins(opts.args)
@@ -36,14 +49,23 @@ end, {})
 -- Open a scratch buffer
 vim.api.nvim_create_user_command('Scratch', function()
   vim.cmd('belowright 10new')
-  local buf = vim.api.nvim_get_current_buf()
-  for name, value in pairs {
-    filetype = 'scratch',
-    buftype = 'nofile',
-    bufhidden = 'hide',
-    swapfile = false,
-    modifiable = true,
-  } do
-  vim.api.nvim_set_option_value(name, value, { buf = buf })
-end
+  scratch_buf_init()
 end, { nargs = 0 })
+
+-- Messages redirection
+-- 1. Redirect messages to a file
+vim.api.nvim_create_user_command('MsgsFile', function()
+  local file = '$HOME/Downloads/messages.txt'
+  vim.cmd('redir >> ' .. file .. ' | silent messages | redir END')
+end, {})
+-- 2. Redirect messages to system clipboard
+vim.api.nvim_create_user_command('MsgsClip', function()
+  vim.cmd('redir @*>  | silent messages | redir END')
+end, {})
+-- 3. Redirect messages to a split window
+vim.api.nvim_create_user_command('MsgsSplit', function()
+  vim.cmd('vertical new')
+  scratch_buf_init()
+  vim.cmd('redir => msg_output | silent messages | redir END')
+  vim.cmd("execute 'put =msg_output'")
+end, {})

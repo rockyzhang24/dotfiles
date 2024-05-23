ccb1b6a2e459406bd1010a23b16024e922942d1d
Rocky Zhang <yzhang.rocky@gmail.com>
2024-05-23 15:07:23 -0700

    nvim(iswap): update config and keymap

--- .config/nvim/lua/rockyz/plugins/iswap.lua
+++ .config/nvim/lua/rockyz/plugins/iswap.lua
@@ -1,2 +1,8 @@
--- Use cx to be consistent with vim-exchange
-vim.keymap.set('n', '<Leader>cx', '<Cmd>ISwap<CR>')
+require('iswap').setup({
+  keys = 'asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM1234567890',
+  grey = 'disable',
+})
+
+-- Use cx as the prefix for keymaps regarding exchange. It's consistent with tommcdo/vim-exchange. i
+-- for "interactively".
+vim.keymap.set('n', 'cxi', '<Cmd>ISwap<CR>')

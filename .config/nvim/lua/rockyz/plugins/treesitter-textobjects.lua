19085809cc34f5dcc70caec7323e8b2e5531c9d4
Rocky Zhang <yzhang.rocky@gmail.com>
2024-05-23 20:52:17 -0700

    nvim(treesitter-textobj): add call textobj, remove unused ones

--- .config/nvim/lua/rockyz/plugins/treesitter-textobjects.lua
+++ .config/nvim/lua/rockyz/plugins/treesitter-textobjects.lua
@@ -1,56 +1,33 @@
--- k for class (c, l, a and s have been occupied already)
--- a for parameter (argument)
--- i for condition (if)
--- o for loop
-
 require('nvim-treesitter.configs').setup({
   textobjects = {
     select = {
       enable = true,
       lookahead = true,
       keymaps = {
-        ['ak'] = '@class.outer',
-        ['ik'] = '@class.inner',
         ['af'] = '@function.outer',
         ['if'] = '@function.inner',
-        ['aa'] = '@parameter.outer',
-        ['ia'] = '@parameter.inner',
-        ['ai'] = '@conditional.outer',
-        ['ii'] = '@conditional.inner',
-        ['ao'] = '@loop.outer',
-        ['io'] = '@loop.inner',
+        -- ['aa'] = '@parameter.outer',
+        -- ['ia'] = '@parameter.inner',
+        ['av'] = '@call.outer', -- v for invoke
+        ['iv'] = '@call.inner',
       },
     },
     move = {
       enable = true,
       set_jumps = true,
       goto_next_start = {
-        [']k'] = '@class.outer',
         [']f'] = '@function.outer',
         [']a'] = '@parameter.inner',
-        [']i'] = '@conditional.outer',
-        [']o'] = '@loop.outer',
+        [']v'] = '@call.outer',
       },
       goto_next_end = {
-        [']K'] = '@class.outer',
-        [']F'] = '@function.outer',
-        [']A'] = '@parameter.inner',
-        [']I'] = '@conditional.outer',
-        [']O'] = '@loop.outer',
       },
       goto_previous_start = {
-        ['[k'] = '@class.outer',
         ['[f'] = '@function.outer',
         ['[a'] = '@parameter.inner',
-        ['[i'] = '@conditional.outer',
-        ['[o'] = '@loop.outer',
+        ['[v'] = '@call.outer',
       },
       goto_previous_end = {
-        ['[K'] = '@class.outer',
-        ['[F'] = '@function.outer',
-        ['[A'] = '@parameter.inner',
-        ['[I'] = '@conditional.outer',
-        ['[O'] = '@loop.outer',
       },
     },
   },

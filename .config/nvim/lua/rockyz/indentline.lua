8c6f37c52bd91987e2a886ff62f89ccfdc84d3ac
Rocky Zhang <yzhang.rocky@gmail.com>
2024-06-14 23:56:35 -0700

    nvim: simplify indentline I was so stupid and I even overlooked vim.opt:append()

--- .config/nvim/lua/rockyz/indentline.lua
+++ .config/nvim/lua/rockyz/indentline.lua
@@ -6,21 +6,9 @@
 -- 2. If using tab as indentation: set leadmultispace to a special character for denotation and tab
 --    to the indent line character.
 
-local function set_or_update(is_local)
-
-  local indentchar_update = function(items)
-    local listchars = vim.api.nvim_get_option_value('listchars', {})
-    for item, val in pairs(items) do
-      if listchars:match(item) then
-        listchars = listchars:gsub('(' .. item .. ':)[^,]*', '%1' .. val)
-      else
-        listchars = listchars .. ',' .. item .. ':' .. val
-      end
-    end
-    return listchars
-  end
-
-  local new_chars = ''
+local function indentchar_update(is_local)
+  local tab
+  local leadmultispace
   if vim.api.nvim_get_option_value('expandtab', {}) then
     -- For space indentation
     local spaces = vim.api.nvim_get_option_value('shiftwidth', {})
@@ -28,39 +16,31 @@ local function set_or_update(is_local)
     if spaces == 0 then
       spaces = vim.api.nvim_get_option_value('tabstop', {})
     end
-    new_chars = indentchar_update({
-      tab = '› ',
-      leadmultispace = vim.g.indentline_char .. string.rep(' ', spaces - 1),
-    })
+    tab = '› '
+    leadmultispace = vim.g.indentline_char .. string.rep(' ', spaces - 1)
   else
     -- For tab indentation
-    new_chars = indentchar_update({
-      tab = vim.g.indentline_char .. ' ',
-      leadmultispace = '␣'
-    })
+    tab = vim.g.indentline_char .. ' '
+    leadmultispace = '␣'
   end
-  local opts = {}
-  if is_local then
-    opts.scope = 'local'
-  end
-  vim.api.nvim_set_option_value('listchars', new_chars, opts)
+
+  -- Update
+  local opt = is_local and vim.opt_local or vim.opt
+  opt.listchars:append({ tab = tab })
+  opt.listchars:append({ leadmultispace = leadmultispace })
 end
 
 vim.api.nvim_create_augroup('rockyz/indentline', { clear = true })
-
--- Initialize the indent line
 vim.api.nvim_create_autocmd({ 'VimEnter' }, {
   group = 'rockyz/indentline',
   callback = function()
-    set_or_update(false)
+    indentchar_update(false)
   end,
 })
--- Update the indent line
 vim.api.nvim_create_autocmd({ 'OptionSet' }, {
   group = 'rockyz/indentline',
   pattern = { 'shiftwidth', 'expandtab', 'tabstop' },
   callback = function()
-    set_or_update(vim.v.option_type == 'local')
+    indentchar_update(vim.v.option_type == 'local')
   end,
 })
-

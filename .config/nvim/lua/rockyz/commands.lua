a4c17dbc08dfd508d00cabfce7c96749b7afc21a
Rocky Zhang <yzhang.rocky@gmail.com>
2024-06-16 00:08:17 -0700

    nvim: add a command to copy text using code block format Ref: https://www.reddit.com/r/neovim/comments/1dfvluw/comment/l8md8wv/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button

--- .config/nvim/lua/rockyz/commands.lua
+++ .config/nvim/lua/rockyz/commands.lua
@@ -62,3 +62,15 @@ vim.api.nvim_create_user_command('MsgsSplit', function()
   vim.cmd('redir => msg_output | silent messages | redir END')
   vim.cmd("execute 'put =msg_output'")
 end, {})
+
+-- Copy text to clipboard using code block format, i.e.,
+-- ```{ft}
+-- {content}
+-- ```
+vim.api.nvim_create_user_command('CopyCodeBlock', function(opts)
+  local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
+  local content = table.concat(lines, '\n')
+  local result = string.format('```%s\n%s\n```', vim.bo.filetype, content)
+  vim.fn.setreg('+', result)
+  vim.notify 'Text copied to clipboard'
+end, { range = true })

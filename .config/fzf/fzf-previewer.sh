5f55bf57c022845708fc88a680264518dc31edf3
Rocky Zhang <yzhang.rocky@gmail.com>
2024-06-24 16:55:34 -0700

    fzf,lf: previewing media files by showing info is meaningless

--- .config/fzf/fzf-previewer.sh
+++ .config/fzf/fzf-previewer.sh
@@ -14,29 +14,26 @@ fi
 
 # In kitty, preview image via kitty image protocal. Introduced since fzf 0.43.0. It works in the
 # normal terminal and tmux but not in Neovim's builtin term.
-# Use file command as the fallback. It outputs the file properties, separated by comma, in a single
-# lone line. In order to fit the width of the preview window, replace each comma with a line break,
-# but leave the commas inside square brackets unchanged.
 if [[ $mime =~ image/ ]]; then
+  echo "Resolution: $(identify -format "%w×%h" "$1")"
   if [[ -n $KITTY_WINDOW_ID && -z $NVIM ]]; then
     # --transfer-mode=memory is the fastest option but if you want fzf to be able to redraw the image
     # on terminal resize or on 'change-preview-window', you need to use --transfer-mode=stream.
-    echo "Resolution: $(identify -format "%w×%h" "$1")"
     kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --align left --place="${FZF_PREVIEW_COLUMNS}x$(( FZF_PREVIEW_LINES - 1 ))"@0x0 "$1" | sed '$d' | sed $'$s/$/\e[m/'
   else
-    file "$1" | gsed -r ':a; s/(\[[^][]*),([^][]*\])/\1TTEEMMPP\2/g; ta; s/, /\n/g; s/TTEEMMPP/,/g'
+    echo "Image preview is NOT supported!"
   fi
   exit
 fi
 # Video can be previewed by previewing its thumbnail
 if [[ $mime =~ video/|audio/ ]]; then
+  dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$1")
+  echo "Dimensions: $dimensions"
   if [[ -n $KITTY_WINDOW_ID && -z $NVIM ]]; then
-    dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$1")
-    echo "Dimensions: $dimensions"
     thumbnail=$($HOME/.config/lf/vidthumb "$1")
     kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --align left --place="${FZF_PREVIEW_COLUMNS}x$(( FZF_PREVIEW_LINES - 1))"@0x0 "$thumbnail" | sed '$d' | sed $'$s/$/\e[m/'
   else
-    file "$1" | gsed -r ':a; s/(\[[^][]*),([^][]*\])/\1TTEEMMPP\2/g; ta; s/, /\n/g; s/TTEEMMPP/,/g'
+    echo "Video preview is NOT supported!"
   fi
   exit
 fi

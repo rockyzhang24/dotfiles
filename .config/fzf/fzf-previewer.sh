a48aaa5443911e6a979719e66f69303e41b0363a
Rocky Zhang <yzhang.rocky@gmail.com>
2024-06-28 22:32:29 -0700

    polish bash code

--- .config/fzf/fzf-previewer.sh
+++ .config/fzf/fzf-previewer.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
-if [[ $(file -b "$1") == directory ]]; then
-  tree -C "$1"
+path="${1/#\~/$HOME}"
+
+if [[ $(file -b "$path") == directory ]]; then
+  tree -C "$path"
   exit
 fi
 
-mime=$(file --dereference --brief --mime-type "$1")
+mime=$(file --dereference --brief --mime-type "$path")
 
 if [[ $mime =~ \-binary ]]; then
-  file "$1"
+  file "$path"
   exit
 fi
 
 # In kitty, preview image via kitty image protocal. Introduced since fzf 0.43.0. It works in the
 # normal terminal and tmux but not in Neovim's builtin term.
 if [[ $mime =~ image/ ]]; then
-  echo "Resolution: $(identify -format "%w×%h" "$1")"
+  echo "Resolution: $(identify -format "%w×%h" "$path")"
   if [[ -n $KITTY_WINDOW_ID && -z $NVIM ]]; then
     # --transfer-mode=memory is the fastest option but if you want fzf to be able to redraw the image
     # on terminal resize or on 'change-preview-window', you need to use --transfer-mode=stream.
-    kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --align left --place="${FZF_PREVIEW_COLUMNS}x$(( FZF_PREVIEW_LINES - 1 ))"@0x0 "$1" | sed '$d' | sed $'$s/$/\e[m/'
+    kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --align left --place="${FZF_PREVIEW_COLUMNS}x$(( FZF_PREVIEW_LINES - 1 ))"@0x0 "$path" | sed '$d' | sed $'$s/$/\e[m/'
   else
     echo "Image preview is NOT supported!"
   fi
@@ -27,10 +29,10 @@ if [[ $mime =~ image/ ]]; then
 fi
 # Video can be previewed by previewing its thumbnail
 if [[ $mime =~ video/|audio/ ]]; then
-  dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$1")
+  dimensions=$(ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$path")
   echo "Dimensions: $dimensions"
   if [[ -n $KITTY_WINDOW_ID && -z $NVIM ]]; then
-    thumbnail=$($HOME/.config/lf/vidthumb "$1")
+    thumbnail=$("$HOME"/.config/lf/vidthumb "$path")
     kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --align left --place="${FZF_PREVIEW_COLUMNS}x$(( FZF_PREVIEW_LINES - 1))"@0x0 "$thumbnail" | sed '$d' | sed $'$s/$/\e[m/'
   else
     echo "Video preview is NOT supported!"
@@ -38,7 +40,7 @@ if [[ $mime =~ video/|audio/ ]]; then
   exit
 fi
 
-(bat --color=always --style=numbers "$1" \
-  || highlight --out-format truecolor --style darkplus --force --line-numbers "$1" \
-  || cat "$1") | head -200 \
-  || echo -e " No preview supported for the current selection:\n\n $1"
+(bat --color=always --style=numbers "$path" \
+  || highlight --out-format truecolor --style darkplus --force --line-numbers "$path" \
+  || cat "$path") | head -200 \
+  || echo -e " No preview supported for the current selection:\n\n $path"

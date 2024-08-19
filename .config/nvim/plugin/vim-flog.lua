b959e06996a0e7a20a235f629ff8bb09143440ef
Rocky Zhang <yzhang.rocky@gmail.com>
2024-08-19 14:07:16 -0700

    nvim and kitty: kitty 0.36.0 supports rendering special symbols for drawing commit graph in vim-flog Ref: https://github.com/kovidgoyal/kitty/pull/7681

--- .config/nvim/plugin/vim-flog.lua
+++ .config/nvim/plugin/vim-flog.lua
@@ -2,5 +2,10 @@ vim.g.flog_default_opts = {
   max_count = 1000,
 }
 
+-- Use special font symbols to render git commit graph. It's supported by kitty since 0.36.0
+if vim.env.KITTY_WINDOW_ID then
+  vim.g.flog_enable_extended_chars = true
+end
+
 vim.keymap.set('n', ',L', '<Cmd>Flog<CR>')
 vim.keymap.set('n', ',l', '<Cmd>Flog -raw-args=--follow -path=%<CR>') -- git log for current file

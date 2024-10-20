eeddcdc2fc137af08c0ea5a38669dfc8cc01aa6b
Rocky Zhang <yzhang.rocky@gmail.com>
2024-10-20 00:00:10 -0700

    nvim(cmp): go back to use hrsh7th/nvim-cmp cuz that perf pr got merged

--- .config/nvim/lua/rockyz/minpac.lua
+++ .config/nvim/lua/rockyz/minpac.lua
@@ -1,96 +1,92 @@
+local plugins = {
+    {
+        'k-takata/minpac',
+        { type = 'opt' },
+    },
+    'nvim-lua/plenary.nvim',
+    'kyazdani42/nvim-web-devicons',
+    'dstein64/vim-startuptime',
+    'tpope/vim-surround',
+    'tpope/vim-sleuth',
+    'tpope/vim-repeat',
+    'RRethy/vim-illuminate',
+    'uga-rosa/ccc.nvim',
+    'godlygeek/tabular',
+    'mbbill/undotree',
+    'mhinz/vim-grepper',
+    'haya14busa/vim-asterisk',
+    'tommcdo/vim-exchange',
+    -- cx{motion}, cxx (line), X (visual), cxc (clear), and `.` is supported
+    'othree/eregex.vim',
+    'wellle/targets.vim',
+    'unblevable/quick-scope',
+    'danymat/neogen',
+    'stevearc/conform.nvim',
+    'Wansmer/treesj',
+    'willothy/flatten.nvim',
+    'michaeljsmith/vim-indent-object',
+    -- ii for inner indentation, ai for indentation and one line above, aI for indentation and
+    -- lines above and below
+    'preservim/tagbar',
+    'dhananjaylatkar/cscope_maps.nvim',
+
+    -- Fuzzy Finder
+    {
+        'junegunn/fzf',
+        { ['do'] = 'packloadall! | call fzf#install()' },
+    },
+    'junegunn/fzf.vim',
+    'nvim-telescope/telescope.nvim',
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        { ['do'] = 'make' },
+    },
+    'ibhagwan/fzf-lua',
+
+    -- LSP
+    'neovim/nvim-lspconfig',
+    'SmiteshP/nvim-navic',
+    'smjonas/inc-rename.nvim',
+    'b0o/SchemaStore.nvim',
+    'stevearc/aerial.nvim',
+
+    -- Autocomplete
+    'hrsh7th/nvim-cmp',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+
+    -- Snippets
+    {
+        'L3MON4D3/LuaSnip',
+        { ['do'] = 'make install_jsregexp' },
+    },
+    'saadparwaiz1/cmp_luasnip',
+
+    -- Treesitter
+    {
+        'nvim-treesitter/nvim-treesitter',
+        { ['do'] = 'TSUpdate' },
+    },
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    'mizlan/iswap.nvim',
+    'm-demare/hlargs.nvim',
+
+    -- Git
+    'lewis6991/gitsigns.nvim',
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb', -- :GBrowse
+    'rbong/vim-flog',
+}
+
 local function pack_init()
-    local plugins = {
-        {
-            'k-takata/minpac',
-            { type = 'opt' },
-        },
-        'nvim-lua/plenary.nvim',
-        'kyazdani42/nvim-web-devicons',
-        'dstein64/vim-startuptime',
-        'tpope/vim-surround',
-        'tpope/vim-sleuth',
-        'tpope/vim-repeat',
-        'RRethy/vim-illuminate',
-        'uga-rosa/ccc.nvim',
-        'godlygeek/tabular',
-        'mbbill/undotree',
-        'mhinz/vim-grepper',
-        'haya14busa/vim-asterisk',
-        'tommcdo/vim-exchange',
-        -- cx{motion}, cxx (line), X (visual), cxc (clear), and `.` is supported
-        'othree/eregex.vim',
-        'wellle/targets.vim',
-        'unblevable/quick-scope',
-        'danymat/neogen',
-        'stevearc/conform.nvim',
-        'Wansmer/treesj',
-        'willothy/flatten.nvim',
-        'michaeljsmith/vim-indent-object',
-        -- ii for inner indentation, ai for indentation and one line above, aI for indentation and
-        -- lines above and below
-        'preservim/tagbar',
-        'dhananjaylatkar/cscope_maps.nvim',
-
-        -- Fuzzy Finder
-        {
-            'junegunn/fzf',
-            { ['do'] = 'packloadall! | call fzf#install()' },
-        },
-        'junegunn/fzf.vim',
-        'nvim-telescope/telescope.nvim',
-        {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            { ['do'] = 'make' },
-        },
-        'ibhagwan/fzf-lua',
-
-        -- LSP
-        'neovim/nvim-lspconfig',
-        'SmiteshP/nvim-navic',
-        'smjonas/inc-rename.nvim',
-        'b0o/SchemaStore.nvim',
-        'stevearc/aerial.nvim',
-
-        -- Autocomplete
-        {
-            'iguanacucumber/magazine.nvim',
-            { name = 'nvim-cmp' },
-        },
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-
-        -- Snippets
-        {
-            'L3MON4D3/LuaSnip',
-            { ['do'] = 'make install_jsregexp' },
-        },
-        'saadparwaiz1/cmp_luasnip',
-
-        -- Treesitter
-        {
-            'nvim-treesitter/nvim-treesitter',
-            { ['do'] = 'TSUpdate' },
-        },
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        'mizlan/iswap.nvim',
-        'm-demare/hlargs.nvim',
-
-        -- Git
-        'lewis6991/gitsigns.nvim',
-        'tpope/vim-fugitive',
-        'tpope/vim-rhubarb', -- :GBrowse
-        'rbong/vim-flog',
-    }
-
     vim.cmd.packadd('minpac')
     vim.fn['minpac#init']({
         progress_open = 'vertical',
         status_open = 'vertical',
         status_auto = true,
     })
-
     for _, plugin in ipairs(plugins) do
         if type(plugin) == 'string' then
             vim.fn['minpac#add'](plugin)

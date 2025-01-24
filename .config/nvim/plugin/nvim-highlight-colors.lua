87c9c170094bd0575b0edaa990bc68e9351f0625
Rocky Zhang <yzhang.rocky@gmail.com>
2025-01-24 00:18:25 -0800

    nvim(nvim-highlight-colors): disable short hex

--- .config/nvim/plugin/nvim-highlight-colors.lua
+++ .config/nvim/plugin/nvim-highlight-colors.lua
@@ -1,6 +1,7 @@
 require('nvim-highlight-colors').setup({
     render = 'virtual',
     virtual_symbol = '●',
+    enable_short_hex = false,
     enable_tailwind = true,
     exclude_filetypes = {
         'minpac',

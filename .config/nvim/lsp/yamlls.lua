abd351f4ffab6bd35032fc29e716715424f72f10
Rocky Zhang <yzhang.rocky@gmail.com>
2025-08-20 17:24:29 -0700

    nvim(lsp/yamlls): set documentFormattingProvider to true Ref: https://github.com/neovim/nvim-lspconfig/commit/592de750bf5b8c45fc48cebfde585721626ffd6d

--- .config/nvim/lsp/yamlls.lua
+++ .config/nvim/lsp/yamlls.lua
@@ -16,4 +16,11 @@ return {
             schemas = require('schemastore').yaml.schemas(),
         },
     },
+    on_init = function(client)
+        --- https://github.com/neovim/nvim-lspconfig/pull/4016
+        --- Since formatting is disabled by default if you check `client:supports_method('textDocument/formatting')`
+        --- during `LspAttach` it will return `false`. This hack sets the capability to `true` to facilitate
+        --- autocmd's which check this capability
+        client.server_capabilities.documentFormattingProvider = true
+    end,
 }

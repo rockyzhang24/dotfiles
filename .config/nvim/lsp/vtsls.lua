22ff64c59af9faca1a89547101cab9430805b54f
Rocky Zhang <yzhang.rocky@gmail.com>
2025-08-20 21:16:06 -0700

    nvim(lsp/vtsls): give root markers equal priority Ref: https://github.com/neovim/nvim-lspconfig/commit/99748c019f214d2f4372094fe5766a616f27e124

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -1,3 +1,22 @@
+-- Ref:
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lsp/vtsls.lua
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#vtsls
+--
+-- To configure a TypeScript project, add a
+-- [`tsconfig.json`](https://www.typescriptlang.org/docs/handbook/tsconfig-json.html)
+-- or [`jsconfig.json`](https://code.visualstudio.com/docs/languages/jsconfig) to
+-- the root of your project.
+--
+-- Monorepo support
+--
+-- `vtsls` supports monorepos by default. It will automatically find the `tsconfig.json` or
+-- `jsconfig.json` corresponding to the package you are working on. This works without the need of
+-- spawning multiple instances of `vtsls`, saving memory.
+--
+-- It is recommended to use the same version of TypeScript in all packages, and therefore have it
+-- available in your workspace root. The location of the TypeScript binary will be determined
+-- automatically, but only once.
+
 local lang_settings = {
     suggest = { completeFunctionCalls = true },
     inlayHints = {
@@ -17,7 +36,20 @@ return {
         'typescriptreact',
         'typescript.tsx',
     },
-    root_markers = { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' },
+    root_dir = function(bufnr, on_dir)
+        -- The project root is where the LSP can be started from
+        -- As stated in the documentation above, this LSP supports monorepos and simple projects.
+        -- We select then from the project root, which is identified by the presence of a package
+        -- manager lock file.
+        local project_root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+        -- Give the root markers equal priority by wrapping them in a table
+        local project_root = vim.fs.root(bufnr, { project_root_markers })
+        if not project_root then
+            return
+        end
+
+        on_dir(project_root)
+    end,
     settings = {
         -- See the configuration schema
         -- https://github.com/yioneko/vtsls/blob/main/packages/service/configuration.schema.json

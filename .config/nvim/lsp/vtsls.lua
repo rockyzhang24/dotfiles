34502b8a1f4e5b0eabbf748077983a52dce64f18
Rocky Zhang <yzhang.rocky@gmail.com>
2025-09-04 11:32:44 -0700

    nvim(lsp/vtsls): add deno.lock in root markers

--- .config/nvim/lsp/vtsls.lua
+++ .config/nvim/lsp/vtsls.lua
@@ -44,9 +44,9 @@ return {
         -- As stated in the documentation above, this LSP supports monorepos and simple projects.
         -- We select then from the project root, which is identified by the presence of a package
         -- manager lock file.
-        local project_root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+        local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock', 'deno.lock' }
         -- Give the root markers equal priority by wrapping them in a table
-        local project_root = vim.fs.root(bufnr, { project_root_markers })
+        local project_root = vim.fs.root(bufnr, { root_markers })
         if not project_root then
             return
         end

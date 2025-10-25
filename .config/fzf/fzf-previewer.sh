c7455e82567ef6ae71c2198f08d5630bb6a12238
Rocky Zhang <yzhang.rocky@gmail.com>
2025-10-24 20:50:16 -0700

    fzf/lf: update image previewer

--- .config/fzf/fzf-previewer.sh
+++ .config/fzf/fzf-previewer.sh
@@ -20,37 +20,22 @@ if [[ $mime =~ \-binary ]]; then
 fi
 
 image_previewer() {
-    #
-    # In Kitty
-    # Kitty image protocal works well in both normal terminal and tmux, but not in Neovim's builtin
-    # terminal. Tmux is special, it works in tmux pane but fails in tmux popup (ref:
-    # https://github.com/junegunn/fzf/issues/3972). My fzf starts in tmux popup when it runs in
-    # tmux, so kitty image protocal does not work. In all the situations where kitty image protocal
-    # fails to work, use chafa with symbols (ANSI art) format as a workaround.
-    #
-    # For Ghostty, since it uses the Kitty graphic protocol, we handle image preview in the same way
-    # as Kitty.
-    #
-    if [[ $KITTY_WINDOW_ID || $GHOSTTY_BIN_DIR ]]; then
-        # In Kitty or Ghostty
-        if [[ ! $TERM =~ "tmux" && -z $NVIM ]]; then
-            # Not in tmux and not in neovim's builtin terminal
+    # kitty's icat works only in normal tmux pane but fails in tmux popup (see:
+    # https://github.com/junegunn/fzf/issues/3972 and https://github.com/tmux/tmux/issues/4329)
+    if [[ ! $TERM =~ "tmux" && -z $NVIM ]]; then
+        if [[ $KITTY_WINDOW_ID || $GHOSTTY_BIN_DIR ]]; then
             kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --align left --place="${FZF_PREVIEW_COLUMNS}x$((FZF_PREVIEW_LINES - 1))"@0x0 "$1" | sed '$d' | sed $'$s/$/\e[m/'
-            # --transfer-mode=memory is the fastest option but if we want fzf to be able to redraw the image
-            # on terminal resize or on 'change-preview-window', we need to use --transfer-mode=stream.
-        else
-            printf "Use Chafa's symbols (ANSI art) format to preview images.\n"
-            chafa -f symbols -s "${FZF_PREVIEW_COLUMNS}x$((FZF_PREVIEW_LINES - 1))" --animate off --polite on "$1"
-        fi
-    elif [[ $WEZTERM_PANE || $ALACRITTY_WINDOW_ID ]]; then
-        # In Wezterm or Alacritty
-        if [[ -z $NVIM ]]; then
+            # NOTE:
+            # --transfer-mode=memory is the fastest option but if we want fzf to be able to redraw
+            # the image on terminal resize or on 'change-preview-window', we need to use
+            # --transfer-mode=stream.
+        elif [[ $WEZTERM_PANE || $ALACRITTY_WINDOW_ID ]]; then
             chafa -s "${FZF_PREVIEW_COLUMNS}x$((FZF_PREVIEW_LINES - 1))" --animate off --polite on "$1"
         else
-            chafa -f symbols -s "${FZF_PREVIEW_COLUMNS}x$((FZF_PREVIEW_LINES - 1))" --animate off --polite on "$1"
+            printf "Image preview is not supported"
         fi
     else
-        echo "Image preview is NOT supported!"
+        printf "Image preview is not supported"
     fi
 }
 

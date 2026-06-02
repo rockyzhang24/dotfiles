b3cfe880b1a84b61eaeca4e3894694121bbdab8a
Rocky Zhang <yzhang.rocky@gmail.com>
2026-06-02 12:50:48 -0700

    refactor(nvim/utils): simplify and clean `get_lines()`

--- .config/nvim/lua/rockyz/utils/api.lua
+++ .config/nvim/lua/rockyz/utils/api.lua
@@ -3,84 +3,58 @@ local M = {}
 ---This is the get_lines() function from https://github.com/neovim/neovim/blob/master/runtime/lua/vim/lsp/util.lua
 ---
 ---Gets the zero-indexed lines from the given buffer.
----Works on unloaded buffers by reading the file using libuv to bypass buf reading events.
+---Works on unloaded buffers by reading the file and bypass buf reading events.
 ---Falls back to loading the buffer and nvim_buf_get_lines for buffers with non-file URI.
 ---
----@param bufnr integer bufnr to get lines from
+---@param buf integer buffer handle to get the lines from
 ---@param rows integer[] zero-indexed line numbers
 ---@return table<integer, string>|string # A table mapping rows to lines
-function M.get_lines(bufnr, rows)
-    if bufnr == nil or bufnr == 0 then
-        bufnr = vim.api.nvim_get_current_buf()
-    end
-
+function M.get_lines(buf, rows)
     local function buf_lines()
-        ---@type table<integer,string>
-        local lines = {}
+        local row_line = {} --- @type table<integer,string>
         for _, row in ipairs(rows) do
-            lines[row] = (vim.api.nvim_buf_get_lines(bufnr, row, row + 1, false) or { '' })[1]
+            row_line[row] = vim.api.nvim_buf_get_lines(buf, row, row + 1, false)[1] or ''
         end
-        return lines
+        return row_line
     end
 
     -- Use loaded buffer if available
-    if vim.fn.bufloaded(bufnr) == 1 then
+    if vim.fn.bufloaded(buf) == 1 then
         return buf_lines()
     end
 
-    local uri = vim.uri_from_bufnr(bufnr)
-
-    -- Load the buffer if this is not a file uri
-    if uri:sub(1, 4) ~= 'file' then
-        vim.fn.bufload(bufnr)
+    -- Load the buffer if this is not a file URI.
+    -- Custom language server protocol extensions can result in servers sending
+    -- URIs with custom schemes. Plugins are able to load these via `BufReadCmd` autocmds.
+    if not vim.startswith(vim.uri_from_bufnr(buf), 'file://') then
+        vim.fn.bufload(buf)
         return buf_lines()
     end
 
-    local filename = vim.api.nvim_buf_get_name(bufnr)
-    if vim.fn.isdirectory(filename) ~= 0 then
-        return {}
-    end
-
-    -- get the data from the file
-    local fd = vim.uv.fs_open(filename, 'r', 438)
-    if not fd then
-        return ''
-    end
-    local stat = assert(vim.uv.fs_fstat(fd))
-    local data = assert(vim.uv.fs_read(fd, stat.size, 0))
-    vim.uv.fs_close(fd)
-
-    ---@type table<integer,true|string> rows we need to retrieve
-    local lines = {}
-    local need = 0 -- keep track of how many unique rows we need
+    local row_line = {} --- @type table<integer, string>
     for _, row in pairs(rows) do
-        if not lines[row] then
-            need = need + 1
-        end
-        lines[row] = true
+        row_line[row] = ''
     end
 
-    local found = 0
-    local lnum = 0
+    -- Get the data from the file.
+    local success, data = pcall(vim.fn.readblob, vim.api.nvim_buf_get_name(buf))
+    if not success then
+        return row_line
+    end
 
+    local need = vim.tbl_count(row_line)
+    local row = 0
     for line in string.gmatch(data, '([^\n]*)\n?') do
-        if lines[lnum] == true then
-            lines[lnum] = line
-            found = found + 1
-            if found == need then
+        if row_line[row] then
+            row_line[row] = line
+            need = need - 1
+            if need == 0 then
                 break
             end
         end
-        lnum = lnum + 1
+        row = row + 1
     end
-
-    -- Change any lines we didn't find to the empty string
-    for i, line in pairs(lines) do
-        if line == true then
-            lines[i] = ''
-        end
-    end
-    return lines --[[@as table<integer,string>]]
+    return row_line
 end
 
 return M

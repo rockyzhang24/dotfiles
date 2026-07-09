946848c84a267d96f4509b614f87a1e1e5c9b862
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-09 00:32:05 -0700

    refactor(nvim/color): clean up background and colorscheme handling

--- .config/nvim/lua/rockyz/color.lua
+++ .config/nvim/lua/rockyz/color.lua
@@ -1,12 +1,12 @@
 vim.o.termguicolors = true
-vim.o.background = 'dark'
+vim.o.background = vim.g.dark_background and 'dark' or 'light'
 
 -- Clear the Normal background when transparent background is enabled
 if vim.g.transparent_background then
-    vim.api.nvim_create_augroup('rockyz.color.bg_clean', { clear = true })
-    vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
-        group = 'rockyz.color.bg_clean',
-        pattern = '*',
+    local clear_bg_augroup = vim.api.nvim_create_augroup('rockyz.color.clear_bg', { clear = true })
+
+    vim.api.nvim_create_autocmd('ColorScheme', {
+        group = clear_bg_augroup,
         callback = function()
             local normal_hl = vim.api.nvim_get_hl(0, { name = 'Normal' })
             vim.api.nvim_set_hl(0, 'Normal', { fg = normal_hl.fg, bg = 'NONE' })
@@ -14,30 +14,30 @@ if vim.g.transparent_background then
     })
 end
 
--- Sync the terminal backgroud to remove the frame around vim which appears if vim's normal
--- backgroud color differs from what is used in terminal itself.
--- Ref:
+-- Sync the terminal background to remove the frame around Vim
+-- This happens when Vim's Normal background differs from the terminal background
+-- References:
 -- https://www.reddit.com/r/neovim/comments/1ehidxy/you_can_remove_padding_around_neovim_instance/
 -- https://github.com/neovim/neovim/issues/16572#issuecomment-1954420136
-local modified = false
-local sync_term_bg_group = vim.api.nvim_create_augroup('rockyz.color.sync_term_bg', { clear = true })
+local terminal_background_synced = false
+local sync_terminal_bg_augroup = vim.api.nvim_create_augroup('rockyz.color.sync_terminal_bg', { clear = true })
 vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
-    group = sync_term_bg_group,
+    group = sync_terminal_bg_augroup,
     callback = function()
         local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
         if normal.bg then
             io.write(string.format('\027]11;#%06x\027\\', normal.bg))
-            modified = true
+            terminal_background_synced = true
         end
     end,
 })
 vim.api.nvim_create_autocmd('UILeave', {
-    group = sync_term_bg_group,
+    group = sync_terminal_bg_augroup,
     callback = function()
-        if modified then
+        if terminal_background_synced then
             io.write('\027]111\027\\')
         end
     end,
 })
 
-vim.cmd('colorscheme ' .. vim.g.colorscheme)
+vim.cmd('colorscheme ' .. vim.fn.fnameescape(vim.g.colorscheme))

d8a698ede9da315ce1daec03ac17eedab1e0ac1a
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-09 01:22:42 -0700

    refactor(nvim/yank): clarify saved view state handling

--- .config/nvim/lua/rockyz/yank.lua
+++ .config/nvim/lua/rockyz/yank.lua
@@ -2,41 +2,44 @@
 
 local M = {}
 
-local saved_win_view
-local winid
-local bufnr
-local report
+local saved_view
+local saved_winid
+local saved_bufnr
+local saved_report
 
 function M.save_win_view()
-    saved_win_view = vim.fn.winsaveview()
-    winid = vim.api.nvim_get_current_win()
-    bufnr = vim.api.nvim_get_current_buf()
-    report = vim.o.report
+    saved_view = vim.fn.winsaveview()
+    saved_winid = vim.api.nvim_get_current_win()
+    saved_bufnr = vim.api.nvim_get_current_buf()
+    saved_report = vim.o.report
     vim.o.report = 65535
 end
 
-function M.clear_win_view()
-    saved_win_view = nil
-    winid = nil
-    bufnr = nil
-    if report then
-        vim.o.report = report
-        report = nil
+function M.clear_saved_state()
+    saved_view = nil
+    saved_winid = nil
+    saved_bufnr = nil
+    if saved_report ~= nil then
+        vim.o.report = saved_report
+        saved_report = nil
     end
 end
 
 function M.restore()
-    if vim.v.operator == 'y' and saved_win_view and vim.api.nvim_get_current_win() == winid and vim.api.nvim_get_current_buf() == bufnr then
-        vim.fn.winrestview(saved_win_view)
+    local is_yank = vim.v.operator == 'y'
+    local is_same_win = vim.api.nvim_get_current_win() == saved_winid
+    local is_same_buf = vim.api.nvim_get_current_buf() == saved_bufnr
+
+    if is_yank and saved_view and is_same_win and is_same_buf then
+        vim.fn.winrestview(saved_view)
     end
-    M.clear_win_view()
+
+    M.clear_saved_state()
 end
 
 vim.api.nvim_create_autocmd('TextYankPost', {
     group = vim.api.nvim_create_augroup('rockyz.yank_restore_view', { clear = true }),
-    callback = function()
-        require('rockyz.yank').restore()
-    end,
+    callback = M.restore,
 })
 
 return M

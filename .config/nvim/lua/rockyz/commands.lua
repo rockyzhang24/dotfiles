3d61a23d1eda0108281f71ed00e7e87fb76a5dc0
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-09 00:25:04 -0700

    refactor(nvim/commands): harden path handling and command validation

--- .config/nvim/lua/rockyz/commands.lua
+++ .config/nvim/lua/rockyz/commands.lua
@@ -5,7 +5,7 @@
 -- RedirMsg
 -- CopyCodeBlock
 -- ReorderList
--- Reindent {old} {new}
+-- Reindent {current_indent} {new_indent}
 -- ToggleAutoFormat[!]
 -- CopyPath [nameonly|relative|absolute]
 -- DiffOrig
@@ -16,8 +16,9 @@
 -- LspRestart
 
 local notify = require('rockyz.utils.notify')
+local win_utils = require('rockyz.utils.win')
 
-local function scratch_buf_init()
+local function init_scratch_buffer(buf)
     for name, value in pairs({
         filetype = 'scratch',
         buftype = 'nofile',
@@ -25,28 +26,27 @@ local function scratch_buf_init()
         swapfile = false,
         modifiable = true,
     }) do
-        vim.api.nvim_set_option_value(name, value, { scope = 'local' })
+        vim.api.nvim_set_option_value(name, value, { buf = buf })
     end
 end
 
 -- Close windows by giving window numbers, e.g., :CloseWin 1 2 3
 vim.api.nvim_create_user_command('CloseWin', function(opts)
-    require('rockyz.utils.win').close_wins(opts.args)
+    win_utils.close_wins(opts.args)
 end, { nargs = '+' })
 
 -- Diff two files side by side in a new tabpage
 -- :DiffSplit <file1> <file2>
 vim.api.nvim_create_user_command('DiffSplit', function(opts)
     if #opts.fargs ~= 2 then
-        vim.api.nvim_echo({
-            { 'ERROR: Require two file names.', 'ErrorMsg' },
-        }, true, {})
-    else
-        vim.cmd('tabedit ' .. vim.fn.fnameescape(opts.fargs[1]))
-        vim.cmd('rightbelow vert diffsplit ' .. vim.fn.fnameescape(opts.fargs[2]))
-        vim.cmd('wincmd p')
-        vim.cmd('normal! gg]c')
+        notify.warn('DiffSplit requires two file names')
+        return
     end
+
+    vim.cmd('tabedit ' .. vim.fn.fnameescape(opts.fargs[1]))
+    vim.cmd('rightbelow vert diffsplit ' .. vim.fn.fnameescape(opts.fargs[2]))
+    vim.cmd('wincmd p')
+    vim.cmd('normal! gg]c')
 end, { nargs = '+', complete = 'file' })
 
 -- Profiler
@@ -60,13 +60,13 @@ end, {})
 -- Open a scratch buffer
 vim.api.nvim_create_user_command('Scratch', function()
     vim.cmd('belowright 10new')
-    scratch_buf_init()
+    init_scratch_buffer(0)
 end, { nargs = 0 })
 
 -- Redirect message output to a split window
 vim.api.nvim_create_user_command('RedirMsg', function()
     vim.cmd('vertical new')
-    scratch_buf_init()
+    init_scratch_buffer(0)
     vim.cmd('redir => msg_output | silent messages | redir END')
     local output = vim.fn.split(vim.g.msg_output, '\n')
     vim.api.nvim_buf_set_lines(0, 0, 0, false, output)
@@ -79,9 +79,11 @@ end, {})
 vim.api.nvim_create_user_command('CopyCodeBlock', function(opts)
     local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
     local content = table.concat(lines, '\n')
-    local result = string.format('```%s\n%s\n```', vim.bo.filetype, content)
-    vim.fn.setreg('+', result)
-    vim.notify('Text copied to clipboard')
+    local filetype = vim.bo.filetype ~= '' and vim.bo.filetype or 'text'
+    local code_block = string.format('```%s\n%s\n```', filetype, content)
+
+    vim.fn.setreg('+', code_block)
+    notify.info('Text copied to clipboard')
 end, { range = true })
 
 -- Reorder numbered list
@@ -98,68 +100,87 @@ vim.api.nvim_create_user_command(
 )
 
 -- Change indentation of the current buffer
--- Usage: `:Reindent cur_indent new_indent`
+-- Usage: `:Reindent current_indent new_indent`
 vim.api.nvim_create_user_command('Reindent', function(opts)
-    if #opts.fargs < 2 then
-        vim.notify('Two arguments are required!')
+    if #opts.fargs ~= 2 then
+        notify.warn('Reindent requires exactly two arguments')
         return
     end
-    local cur_indent, new_indent = tonumber(opts.fargs[1]), tonumber(opts.fargs[2])
-    local prev_et = vim.o.expandtab
+
+    local current_indent = tonumber(opts.fargs[1])
+    local new_indent = tonumber(opts.fargs[2])
+
+    if not current_indent or not new_indent then
+        notify.warn('Reindent requires two numeric arguments')
+        return
+    end
+
+    local original_expandtab = vim.o.expandtab
+
     vim.o.expandtab = false
-    vim.o.tabstop = cur_indent
+    vim.o.tabstop = current_indent
     vim.cmd('retab!')
     vim.o.tabstop = new_indent
-    if prev_et then
+
+    if original_expandtab then
         vim.o.expandtab = true
         vim.cmd('retab!')
     end
+
     vim.o.shiftwidth = new_indent
 end, { nargs = '+' })
 
 -- Toggle variable to switch autoformat (format-on-save)
 -- If [!] is not given, toggle buffer-local autoformat; Use [!] to toggle autoformat globally.
 vim.api.nvim_create_user_command('ToggleAutoFormat', function(opts)
-    local msg = ''
+    local message
     if opts.bang then
         vim.g.autoformat = not vim.g.autoformat
         for _, buf in ipairs(vim.api.nvim_list_bufs()) do
             vim.b[buf].autoformat = false
         end
-        msg = 'Global autoformat (format-on-save) is ' .. (vim.g.autoformat and 'enabled' or 'disabled')
+        message = 'Global autoformat (format-on-save) is ' .. (vim.g.autoformat and 'enabled' or 'disabled')
     else
         vim.b.autoformat = not vim.b.autoformat
-        msg = 'Buffer-local autoformat (format-on-save) is ' .. (vim.b.autoformat and 'enabled' or 'disabled')
+        message = 'Buffer-local autoformat (format-on-save) is ' .. (vim.b.autoformat and 'enabled' or 'disabled')
     end
-    vim.notify(msg, vim.log.levels.INFO)
+    notify.info(message)
 end, { nargs = 0, bang = true })
 
--- Copy file path to clipboard. Support absolute, relative (default) or nameonly.
--- Ref: https://github.com/jdhao/nvim-config/blob/main/plugin/command.lua
-vim.api.nvim_create_user_command('CopyPath', function(context)
-    local full_path = vim.fn.glob('%:p')
-    local file_path = nil
-    if context['args'] == 'nameonly' then
-        file_path = vim.fn.fnamemodify(full_path, ':t')
+-- Copy the current buffer path to clipboard
+-- Supports nameonly, relative (default), and absolute
+-- Reference: https://github.com/jdhao/nvim-config/blob/main/plugin/command.lua
+vim.api.nvim_create_user_command('CopyPath', function(opts)
+    local path_type = opts.args ~= '' and opts.args or 'relative'
+    local full_path = vim.api.nvim_buf_get_name(0)
+
+    if full_path == '' then
+        notify.warn('Current buffer has no file path')
+        return
     end
-    if context['args'] == 'relative' or not context['args'] then
-        local project_marker = { '.git' }
-        local project_root = vim.fs.root(0, project_marker)
-        if project_root == nil then
-            vim.print('Can not find project root!')
+
+    local file_path = ''
+
+    if path_type == 'nameonly' then
+        file_path = vim.fn.fnamemodify(full_path, ':t')
+    elseif path_type == 'relative' then
+        local project_root = vim.fs.root(0, { '.git' })
+        if not project_root then
+            notify.warn('Cannot find project root')
             return
         end
-        project_root = project_root .. '/'
-        file_path = string.gsub(full_path, project_root, '')
-    end
-    if context['args'] == 'absolute' then
+        file_path = vim.fs.relpath(project_root, full_path) or full_path
+    elseif path_type == 'absolute' then
         file_path = full_path
+    else
+        notify.warn('Unsupported path type: ' .. path_type)
+        return
     end
+
     vim.fn.setreg('+', file_path)
-    vim.notify('Filepath copied to clipboard: ' .. file_path, vim.log.levels.INFO)
+    notify.info('File path copied to clipboard: ' .. file_path)
 end, {
-    bang = false,
-    nargs = 1,
+    nargs = '?',
     complete = function()
         return { 'nameonly', 'relative', 'absolute' }
     end,
@@ -177,8 +198,13 @@ vim.api.nvim_create_user_command('Count', function(opts)
     local pattern = vim.fn.escape(opts.args, '/')
     local cmd = string.format('%%s/%s//gn', pattern)
     local view = vim.fn.winsaveview()
-    vim.cmd(cmd)
+
+    local ok, err = pcall(vim.cmd, cmd)
     vim.fn.winrestview(view)
+
+    if not ok then
+        error(err, 0)
+    end
 end, {
     nargs = 1,
 })
@@ -192,7 +218,7 @@ vim.api.nvim_create_user_command('Root', function()
         end
         local root = obj.stdout:gsub('\n$', '')
         vim.schedule(function()
-            vim.cmd('lcd ' .. root)
+            vim.cmd('lcd ' .. vim.fn.fnameescape(root))
             notify.info('Changed directory to: ' .. root)
         end)
     end)
@@ -200,13 +226,13 @@ end, {})
 
 -- LSP commands
 vim.api.nvim_create_user_command('LspInfo', 'checkhealth vim.lsp', {
-    desc = 'Show LSP Info',
+    desc = 'Show LSP info',
 })
 vim.api.nvim_create_user_command('LspLog', function(_)
     local state_path = vim.fn.stdpath('state')
     local log_path = vim.fs.joinpath(state_path, 'lsp.log')
 
-    vim.cmd(string.format('edit %s', log_path))
+    vim.cmd('edit ' .. vim.fn.fnameescape(log_path))
 end, {
     desc = 'Show LSP log',
 })

20f5816f7e9d62a30eac446c12ae2ef79673a20e
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-09 01:46:07 -0700

    refactor(nvim/easymotion): harden getchar handling and label cleanup

--- .config/nvim/lua/rockyz/easymotion.lua
+++ .config/nvim/lua/rockyz/easymotion.lua
@@ -1,50 +1,65 @@
 -- Reference: https://antonk52.github.io/webdevandstuff/post/2025-11-30-diy-easymotion.html
 
-local ns = vim.api.nvim_create_namespace('rockyz.easymotion.labels')
+local label_ns = vim.api.nvim_create_namespace('rockyz.easymotion.labels')
 -- Characters to use as labels. Note: we only use the letters from lower to upper case in ascending
 -- order of how easy to type them in qwerty layout
-local chars = vim.split('fjdkslgha;rueiwotyqpvbcnxmzFJDKSLGHARUEIWOTYQPVBCNXMZ', '')
+local label_chars = vim.split('fjdkslgha;rueiwotyqpvbcnxmzFJDKSLGHARUEIWOTYQPVBCNXMZ', '')
+
+---@return string
+local function getcharstr()
+    local char = vim.fn.getchar()
+    if type(char) == 'number' then
+        return vim.fn.nr2char(char)
+    end
+    return char
+end
 
 local function easy_motion()
-    local char1 = vim.fn.nr2char( vim.fn.getchar() --[[@as number]] )
-    local char2 = vim.fn.nr2char( vim.fn.getchar() --[[@as number]] )
-    local line_idx_start, line_idx_end = vim.fn.line('w0'), vim.fn.line('w$')
+    local first_char = getcharstr()
+    local second_char = getcharstr()
+
+    local first_visible_line = vim.fn.line('w0')
+    local last_visible_line = vim.fn.line('w$')
     local bufnr = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+    vim.api.nvim_buf_clear_namespace(bufnr, label_ns, 0, -1)
 
-    -- to keep track of labels to use
-    local char_idx = 1
-    ---@type table<string, {line: integer, col: integer, id: integer}> Dictionary of extmarks so we can refer back to picked location, from label char to location
-    local extmarks = {}
-    -- lines on the screen
-    local lines = vim.api.nvim_buf_get_lines(bufnr, line_idx_start - 1, line_idx_end, false)
+    -- Keep track of labels to use
+    local label_idx = 1
+    ---@type table<string, {line: integer, col: integer}>
+    local label_positions = {}
+    -- Lines on the screen
+    local lines = vim.api.nvim_buf_get_lines(bufnr, first_visible_line - 1, last_visible_line, false)
 
-    local needle = char1 .. char2
+    local needle = first_char .. second_char
     local is_case_sensitive = needle ~= string.lower(needle)
+
+    if not is_case_sensitive then
+        needle = string.lower(needle)
+    end
 
     for lines_i, line_text in ipairs(lines) do
         if not is_case_sensitive then
             line_text = string.lower(line_text)
         end
-        local line_idx = lines_i + line_idx_start - 1
+        local line_idx = lines_i + first_visible_line - 1
         -- Skip folded lines
         if vim.fn.foldclosed(line_idx) == -1 then
             for i = 1, #line_text do
                 -- Once we find a match, put an extmark there
-                if line_text:sub(i, i + 1) == needle and char_idx <= #chars then
-                    local overlay_char = chars[char_idx]
-                    local linenr = line_idx_start + lines_i - 2
+                if line_text:sub(i, i + 1) == needle and label_idx <= #label_chars then
+                    local overlay_char = label_chars[label_idx]
+                    local linenr = first_visible_line + lines_i - 2
                     local col = i - 1
                     -- `col + 2` to place the extmark after the match
-                    local id = vim.api.nvim_buf_set_extmark(bufnr, ns, linenr, col + 2, {
+                    vim.api.nvim_buf_set_extmark(bufnr, label_ns, linenr, col + 2, {
                         virt_text = { { overlay_char, 'CurSearch' } },
                         virt_text_pos = 'overlay',
                         hl_mode = 'replace',
                     })
-                    -- save the extmark info for quick jumping
-                    extmarks[overlay_char] = { line = linenr, col = col, id = id }
-                    char_idx = char_idx + 1
-                    if char_idx > #chars then
+                    -- Save the label position for quick jumping
+                    label_positions[overlay_char] = { line = linenr, col = col }
+                    label_idx = label_idx + 1
+                    if label_idx > #label_chars then
                         goto break_outer
                     end
                 end
@@ -53,15 +68,19 @@ local function easy_motion()
     end
     ::break_outer::
 
+    if vim.tbl_isempty(label_positions) then
+        return
+    end
+
     vim.schedule(function()
-        local next_char = vim.fn.nr2char(vim.fn.getchar() --[[@as number]])
-        if extmarks[next_char] then
-            local pos = extmarks[next_char]
-            -- to make <C-o> work
+        local next_char = getcharstr()
+        if label_positions[next_char] then
+            local pos = label_positions[next_char]
+            -- Make <C-o> work
             vim.cmd("normal! m'")
             vim.api.nvim_win_set_cursor(0, { pos.line + 1, pos.col })
         end
-        vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+        vim.api.nvim_buf_clear_namespace(bufnr, label_ns, 0, -1)
     end)
 end
 

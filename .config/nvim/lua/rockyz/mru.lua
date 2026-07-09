cec6809e62b8c2a650d0def4f9c24cf8c51bc25e
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-09 14:37:10 -0700

    refactor(nvim/mru): clarify buffer tracking and persistence

--- .config/nvim/lua/rockyz/mru.lua
+++ .config/nvim/lua/rockyz/mru.lua
@@ -1,107 +1,124 @@
 local M = {}
 
 local io_utils = require('rockyz.utils.io')
+local debounce = require('rockyz.utils.debounce')
 
-local db = vim.env.HOME .. '/.mru_file'
-local stat = vim.uv.fs_stat(db)
-if not stat then
-    io.open(db, 'w'):close()
+local mru_db = vim.fs.joinpath(vim.env.HOME, '.mru_file')
+local db_stat = vim.uv.fs_stat(mru_db)
+if not db_stat then
+    local fd = assert(io.open(mru_db, 'w'))
+    fd:close()
 end
 
-local max = 1000
-local bufs = {}
-local tmp_prefix = vim.uv.os_tmpdir()
-local last_bufnr
+local max_entries = 1000
+local flush_threshold = 10
+local flush_delay_ms = 50
+local pending_buffers = {}
+local tmp_dir = vim.uv.os_tmpdir()
+local last_stored_bufnr
+local pending_buffer_count = 0
 
 local ignored_filetypes = {
-    'gitcommit',
+    gitcommit = true,
 }
 
-local function list(db_file)
-    local mru_list = {}
-    local fname_set = { [''] = true }
+local function build_mru_list(db_file)
+    local mru_files = {}
+    local seen_files = { [''] = true }
 
-    local should_add_list = function(fname)
-        if not fname_set[fname] then
-            fname_set[fname] = true
-            local fs_stat = vim.uv.fs_stat(fname)
-            if fs_stat and fs_stat.type ~= 'directory' then
-                return #mru_list < max
-            end
+    local function should_add_file(filename)
+        if seen_files[filename] then
+            return false
         end
-        return false
+
+        seen_files[filename] = true
+        local file_stat = vim.uv.fs_stat(filename)
+        return file_stat ~= nil and file_stat.type ~= 'directory' and #mru_files < max_entries
     end
 
-    while #bufs > 0 do
-        local bufnr = table.remove(bufs)
+    while #pending_buffers > 0 do
+        local bufnr = table.remove(pending_buffers)
         if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].buftype == '' then
-            local fname = vim.api.nvim_buf_get_name(bufnr)
-            if not fname:match(tmp_prefix) then
-                if should_add_list(fname) then
-                    table.insert(mru_list, fname)
-                end
+            local filename = vim.api.nvim_buf_get_name(bufnr)
+            if not vim.startswith(filename, tmp_dir) and should_add_file(filename) then
+                table.insert(mru_files, filename)
             end
         end
     end
 
     local fd = assert(io.open(db_file, 'r'))
-    for fname in fd:lines() do
-        if should_add_list(fname) then
-            table.insert(mru_list, fname)
+    for filename in fd:lines() do
+        if should_add_file(filename) then
+            table.insert(mru_files, filename)
         end
     end
     fd:close()
-    return mru_list
+
+    return mru_files
 end
 
--- Update the mru database and output the mru list
+local function update_mru_file(write_file)
+    local mru_files = build_mru_list(mru_db)
+    pending_buffer_count = 0
+    write_file(mru_db, table.concat(mru_files, '\n'))
+    return mru_files
+end
+
+-- Update the MRU database and return the MRU list
 function M.list()
-    local mru_list = list(db)
-    io_utils.write_file_async(db, table.concat(mru_list, '\n'))
-    return mru_list
+    return update_mru_file(io_utils.write_file_async)
 end
 
-local debounced
+local debounced_flush
+
 function M.flush(force)
-    last_bufnr = nil
+    last_stored_bufnr = nil
+
     if force then
-        io_utils.write_file(db, table.concat(list(db), '\n'))
-    else
-        if not debounced then
-            debounced = require('rockyz.utils.debounce')(function()
-                io_utils.write_file_async(db, table.concat(list(db), '\n'))
-            end, 50, false, true)
-        end
-        debounced()
+        update_mru_file(io_utils.write_file)
+        return
     end
+
+    if not debounced_flush then
+        debounced_flush = debounce(function()
+            update_mru_file(io_utils.write_file_async)
+        end, flush_delay_ms, false, true)
+    end
+    debounced_flush()
 end
 
-local count = 0
 function M.store_buf(bufnr)
     bufnr = bufnr or tonumber(vim.fn.expand('<abuf>', true)) or vim.api.nvim_get_current_buf()
+
+    if not vim.api.nvim_buf_is_valid(bufnr) then
+        return
+    end
+
     if vim.api.nvim_buf_get_name(bufnr) == '' then
         return
     end
+
     local buftype = vim.bo[bufnr].buftype
-    if buftype ~= '' and buftype ~= 'acwrite' or last_bufnr == bufnr then
-        return
-    end
-    local filetype = vim.bo[bufnr].filetype
-    if vim.list_contains(ignored_filetypes, filetype) then
+    local is_supported_buftype = buftype == '' or buftype == 'acwrite'
+    if not is_supported_buftype or last_stored_bufnr == bufnr then
         return
     end
 
-    -- Store the buffer only if it's not in a diff window
-    -- E.g., don't store buffers opened by my own `ngd` shell command
+    local filetype = vim.bo[bufnr].filetype
+    if ignored_filetypes[filetype] then
+        return
+    end
+
+    -- Skip buffers displayed in a diff window, such as those opened by `ngd`
     vim.api.nvim_buf_call(bufnr, function()
         local winid = vim.api.nvim_get_current_win()
-        -- It seems 'diff' setting is delayed so I have to use `vim.schedule` to make it work
+        -- `diff` may be set after BufEnter, so defer the check
         vim.schedule(function()
             if vim.api.nvim_win_is_valid(winid) and not vim.wo[winid].diff then
-                table.insert(bufs, bufnr)
-                last_bufnr = bufnr
-                count = (count + 1) % 10
-                if count == 0 then
+                table.insert(pending_buffers, bufnr)
+                last_stored_bufnr = bufnr
+                pending_buffer_count = pending_buffer_count + 1
+                if pending_buffer_count >= flush_threshold then
                     M.list()
                 end
             end
@@ -109,23 +126,26 @@ function M.store_buf(bufnr)
     end)
 end
 
-vim.api.nvim_create_augroup('rockyz.mru', { clear = true })
+local mru_augroup = vim.api.nvim_create_augroup('rockyz.mru', { clear = true })
+
 vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained' }, {
-    group = 'rockyz.mru',
+    group = mru_augroup,
     callback = function(ev)
-        require('rockyz.mru').store_buf(ev.buf)
+        M.store_buf(ev.buf)
     end,
 })
+
 vim.api.nvim_create_autocmd('VimLeavePre', {
-    group = 'rockyz.mru',
+    group = mru_augroup,
     callback = function()
-        require('rockyz.mru').flush(true)
+        M.flush(true)
     end,
 })
+
 vim.api.nvim_create_autocmd({ 'VimSuspend', 'FocusLost' }, {
-    group = 'rockyz.mru',
+    group = mru_augroup,
     callback = function()
-        require('rockyz.mru').flush()
+        M.flush()
     end,
 })
 

87db0b3ce0146776dafdef2c8e742eef8505be5f
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-09 17:21:52 -0700

    refactor(nvim/utils/io): harden file I/O error handling

--- .config/nvim/lua/rockyz/utils/io.lua
+++ .config/nvim/lua/rockyz/utils/io.lua
@@ -1,47 +1,93 @@
 local M = {}
 
+local file_mode = 438
+
+---@param operation string
+---@param filepath string
+---@param err string
+local function notify_file_error(operation, filepath, err)
+    -- Schedule this to avoid E5560: nvim_exec must not be called in a lua loop callback
+    vim.schedule(function()
+        vim.notify(
+            string.format('Unable to %s file %s, error: %s', operation, filepath, err),
+            vim.log.levels.WARN
+        )
+    end)
+end
+
+---@param fd integer
+---@param filepath string
+---@param callback? fun()
+local function close_file_async(fd, filepath, callback)
+    vim.uv.fs_close(fd, function(err_close)
+        if err_close then
+            notify_file_error('close', filepath, err_close)
+        end
+        if callback then
+            callback()
+        end
+    end)
+end
+
 ---Read a file synchronously
 ---Taken from fzf-lua
+---@param filepath string
+---@return string
 function M.read_file(filepath)
-    local fd = vim.uv.fs_open(filepath, 'r', 438)
-    if fd == nil then
+    local fd = vim.uv.fs_open(filepath, 'r', file_mode)
+    if not fd then
         return ''
     end
-    local stat = assert(vim.uv.fs_stat(filepath))
-    if stat.type ~= 'file' then
+
+    local file_stat = vim.uv.fs_fstat(fd)
+    if not file_stat or file_stat.type ~= 'file' then
+        assert(vim.uv.fs_close(fd))
         return ''
     end
-    local data = assert(vim.uv.fs_read(fd, stat.size, 0))
-    assert(vim.uv.fs_close(fd))
-    return data
+
+    local contents, err_read = vim.uv.fs_read(fd, file_stat.size, 0)
+    local closed, err_close = vim.uv.fs_close(fd)
+    assert(contents, err_read)
+    assert(closed, err_close)
+    return contents
 end
 
 ---Read a file asynchronously
 ---Taken from fzf-lua
----@param callback fun(data: string) The callback accepts the read data as its argument
+---@param filepath string
+---@param callback fun(data: string)
 function M.read_file_async(filepath, callback)
-    vim.uv.fs_open(filepath, 'r', 438, function(err_open, fd)
-        if err_open then
-            -- we must schedule this or we get
-            -- E5560: nvim_exec must not be called in a lua loop callback
-            vim.schedule(function()
-                vim.notify(
-                    string.format('Unable to open file %s, error: %s', filepath, err_open),
-                    vim.log.levels.WARN
-                )
-            end)
+    vim.uv.fs_open(filepath, 'r', file_mode, function(err_open, fd)
+        if err_open or not fd then
+            notify_file_error('open', filepath, err_open or 'unknown error')
+            callback('')
+            return
         end
-        vim.uv.fs_fstat(fd, function(err_fstat, stat)
-            assert(not err_fstat, err_fstat)
-            assert(stat)
-            if stat.type ~= 'file' then
-                return callback('')
+
+        vim.uv.fs_fstat(fd, function(err_fstat, file_stat)
+            if err_fstat or not file_stat then
+                notify_file_error('inspect', filepath, err_fstat or 'unknown error')
+                close_file_async(fd, filepath, function()
+                    callback('')
+                end)
+                return
             end
-            vim.uv.fs_read(fd, stat.size, 0, function(err_read, data)
-                assert(not err_read, err_read)
-                vim.uv.fs_close(fd, function(err_close)
-                    assert(not err_close, err_close)
-                    callback(data)
+
+            if file_stat.type ~= 'file' then
+                close_file_async(fd, filepath, function()
+                    callback('')
+                end)
+                return
+            end
+
+            vim.uv.fs_read(fd, file_stat.size, 0, function(err_read, contents)
+                if err_read then
+                    notify_file_error('read', filepath, err_read)
+                    contents = ''
+                end
+
+                close_file_async(fd, filepath, function()
+                    callback(contents or '')
                 end)
             end)
         end)
@@ -49,28 +95,32 @@ function M.read_file_async(filepath, callback)
 end
 
 ---Write data to a file synchronously
+---@param filepath string
+---@param contents string
 function M.write_file(filepath, contents)
-    local fd = assert(vim.uv.fs_open(filepath, 'w', 438))
-    assert(vim.uv.fs_write(fd, contents, -1))
-    assert(vim.uv.fs_close(fd))
+    local fd = assert(vim.uv.fs_open(filepath, 'w', file_mode))
+    local written, err_write = vim.uv.fs_write(fd, contents, -1)
+    local closed, err_close = vim.uv.fs_close(fd)
+    assert(written, err_write)
+    assert(closed, err_close)
 end
 
 ---Write data to a file asynchronously
+---@param filepath string
+---@param contents string
 function M.write_file_async(filepath, contents)
-    vim.uv.fs_open(filepath, 'w', 438, function(err_open, fd)
-        if err_open then
-            vim.schedule(function()
-                vim.notify(
-                    string.format('Unable to open file %s, error: %s', filepath, err_open),
-                    vim.log.levels.WARN
-                )
-            end)
+    vim.uv.fs_open(filepath, 'w', file_mode, function(err_open, fd)
+        if err_open or not fd then
+            notify_file_error('open', filepath, err_open or 'unknown error')
+            return
         end
-        vim.uv.fs_write(fd, contents, -1, function(err_write, _)
-            assert(not err_write, err_write)
-            vim.uv.fs_close(fd, function(err_close)
-                assert(not err_close, err_close)
-            end)
+
+        vim.uv.fs_write(fd, contents, -1, function(err_write)
+            if err_write then
+                notify_file_error('write', filepath, err_write)
+            end
+
+            close_file_async(fd, filepath)
         end)
     end)
 end

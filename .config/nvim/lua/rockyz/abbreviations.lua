a1fce59d5a57a34c60996eab8146502617570366
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-11 14:47:44 -0700

    refactor(nvim/abbreviations): clarify command-line abbreviations

--- .config/nvim/lua/rockyz/abbreviations.lua
+++ .config/nvim/lua/rockyz/abbreviations.lua
@@ -1,14 +1,22 @@
----Create abbreviation
----@param everywhere? boolean The abbreviation applies everywhere or only at the very beginning.
----Defaults to false.
-local function abbreviate(from, to, everywhere)
-    vim.keymap.set('ca', from, function()
-        return everywhere and to or (vim.fn.getcmdtype() == ':' and vim.fn.getcmdline() == from and to or from)
+---@param lhs string
+---@param rhs string
+---@param anywhere? boolean Whether to expand regardless of command-line position
+local function abbreviate(lhs, rhs, anywhere)
+    vim.keymap.set('ca', lhs, function()
+        if anywhere then
+            return rhs
+        end
+
+        if vim.fn.getcmdtype() == ':' and vim.fn.getcmdline() == lhs then
+            return rhs
+        end
+
+        return lhs
     end, { expr = true })
 end
 
 abbreviate('T', 'tabedit')
-abbreviate('dot', '!git --git-dir=/Users/rockyzhang/dotfiles/ --work-tree=/Users/rockyzhang')
+abbreviate('dot', '!git --git-dir=' .. vim.fs.joinpath(vim.env.HOME, 'dotfiles') .. ' --work-tree=' .. vim.env.HOME)
 abbreviate('ts', 'silent !tmux neww tmux-sessionizer')
 abbreviate('man', 'Man')
 abbreviate('H', 'h')

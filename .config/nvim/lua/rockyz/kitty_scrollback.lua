4fcb56d15389b45a45f80ff1deb41eacab5d8449
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-17 16:31:32 -0700

    refactor(nvim/kitty_scrollback): clean up

--- .config/nvim/lua/rockyz/kitty_scrollback.lua
+++ .config/nvim/lua/rockyz/kitty_scrollback.lua
@@ -1,11 +1,15 @@
 -- Kitty's scrollback pager.
--- It colorizes ANSI escape codes.
+-- It renders ANSI escape sequences.
+--
 -- References:
 -- Folke's config: https://github.com/folke/dot/blob/master/nvim/lua/util/init.lua
 -- Maria's config: https://github.com/MariaSolOs/dotfiles/tree/main/.config/nvim
 -- https://gist.github.com/galaxia4Eva/9e91c4f275554b4bd844b6feece16b3d
 
-return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN)
+---@param input_line_number integer
+---@param cursor_line integer
+---@param cursor_column integer
+return function(input_line_number, cursor_line, cursor_column)
     vim.wo.number = false
     vim.wo.relativenumber = false
     vim.wo.statuscolumn = ''
@@ -14,31 +18,34 @@ return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN)
     vim.opt.listchars = { space = ' ' }
     vim.wo.scrolloff = 0
 
-    local buf = vim.api.nvim_get_current_buf()
+    local bufnr = vim.api.nvim_get_current_buf()
 
-    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-    while #lines > 0 and vim.trim(lines[#lines]) == '' do
-        lines[#lines] = nil
+    local scrollback_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    while #scrollback_lines > 0 and vim.trim(scrollback_lines[#scrollback_lines]) == '' do
+        scrollback_lines[#scrollback_lines] = nil
     end
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, {})
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {})
 
-    vim.api.nvim_chan_send(vim.api.nvim_open_term(buf, {}), table.concat(lines, '\r\n'))
+    local terminal_channel = vim.api.nvim_open_term(bufnr, {})
+    vim.api.nvim_chan_send(terminal_channel, table.concat(scrollback_lines, '\r\n'))
 
-    vim.keymap.set('n', 'q', '<cmd>qa!<cr>', { silent = true, buffer = buf })
+    vim.keymap.set('n', 'q', '<cmd>qa!<cr>', { silent = true, buffer = bufnr })
 
-    local set_cursor = function()
-        vim.api.nvim_feedkeys(tostring(INPUT_LINE_NUMBER) .. [[ggzt]], 'n', true)
-        if CURSOR_LINE ~= 0 and CURSOR_COLUMN ~= 0 then
-            vim.api.nvim_feedkeys(tostring(CURSOR_LINE - 1) .. [[j]], 'n', true)
+    local set_initial_cursor = function()
+        vim.api.nvim_feedkeys(tostring(input_line_number) .. [[ggzt]], 'n', true)
+        if cursor_line ~= 0 and cursor_column ~= 0 then
+            vim.api.nvim_feedkeys(tostring(cursor_line - 1) .. [[j]], 'n', true)
             vim.api.nvim_feedkeys([[0]], 'n', true)
-            vim.api.nvim_feedkeys(tostring(CURSOR_COLUMN - 1) .. [[l]], 'n', true)
+            vim.api.nvim_feedkeys(tostring(cursor_column - 1) .. [[l]], 'n', true)
         end
     end
 
-    local group = vim.api.nvim_create_augroup('rockyz.kitty.scrollback', {})
+    local augroup = vim.api.nvim_create_augroup('rockyz.kitty.scrollback', { clear = true })
+
+    -- Keep Kitty scrollback pager out of terminal mode
     vim.api.nvim_create_autocmd('ModeChanged', {
-        group = group,
-        buffer = buf,
+        group = augroup,
+        buffer = bufnr,
         callback = function()
             local mode = vim.fn.mode()
             if mode == 't' then
@@ -48,7 +55,7 @@ return function(INPUT_LINE_NUMBER, CURSOR_LINE, CURSOR_COLUMN)
     })
 
     vim.schedule(function()
-        set_cursor()
+        set_initial_cursor()
         vim.bo.filetype = 'kitty_scrollback'
     end)
 

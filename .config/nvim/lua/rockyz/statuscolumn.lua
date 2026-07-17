0d6519b821760c5b757672d63c4daec8fae36b9e
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-17 15:08:23 -0700

    refactor(nvim/statuscolumn): improve statuscolumn reference

--- .config/nvim/lua/rockyz/statuscolumn.lua
+++ .config/nvim/lua/rockyz/statuscolumn.lua
@@ -1,8 +1,8 @@
--- UPDATE (10/5/2025): Since the commit
--- https://github.com/neovim/neovim/commit/b6b80824cc71fb9f32ddf2e9a96205633342827e that added
--- "foldinner" suboption value to the "fillchars" option, we don't need the foldfunc below to
--- remove numeric foldlevel, and instead we just need to set foldinner to whitespace.
-
+-- Neovim's `fillchars` option supports the `foldinner` value since 2025-10-05 (commit:
+-- https://github.com/neovim/neovim/commit/b6b80824cc71fb9f32ddf2e9a96205633342827e).
+--
+-- Therefore, this implementation does not need to hide numeric fold levels.
+--
 -- Inspired by luukvbaal/statuscol.nvim
 
 local ffi = require('ffi')
@@ -20,105 +20,135 @@ ffi.cdef([[
     int compute_foldcolumn(win_T *wp, int col); // compute the width of the foldcolumn
 ]])
 
-local error = ffi.new('Error')
+local ffi_error = ffi.new('Error')
 local fillchars = vim.opt_local.fillchars:get()
-local callargs = {}
 
--- Get an args table that will be passed into foldfunc(). The args table is per window and cached in
--- callargs indexed by winid. The structure of the args table shown below:
---
--- {
---     win,
---     wp, -- pointer to win_T
---     fold = {
---         close,
---         open,
---         sep,
---         width,
---     },
---     lnum,
---     relnum,
---     virtnum,
---     cursorline,
--- }
---
-local function get_args()
-    local win = vim.g.statusline_winid
-    local args = callargs[win]
+---@class rockyz.StatuscolumnFold
+---@field close string
+---@field open string
+---@field sep string
+---@field width integer
+
+---@class rockyz.StatuscolumnArgs
+---@field winid integer
+---@field window_ptr ffi.cdata*
+---@field fold rockyz.StatuscolumnFold
+---@field lnum integer
+---@field relnum integer
+---@field virtnum integer
+
+---@type table<integer, rockyz.StatuscolumnArgs>
+local args_by_winid = {}
+
+---Get an args table that will be passed into foldfunc(). The args table is per window and cached in
+---args_by_winid indexed by winid. The structure of the args table shown below:
+---
+---{
+---    winid,
+---    window_ptr, -- pointer to win_T
+---    fold = {
+---        close,
+---        open,
+---        sep,
+---        width,
+---    },
+---    lnum,
+---    relnum,
+---    virtnum,
+---}
+---
+---@return rockyz.StatuscolumnArgs
+local function get_statuscolumn_args()
+    local winid = vim.g.statusline_winid
+    local args = args_by_winid[winid]
+
     if not args then
         args = {
-            win = win,
-            wp = ffi.C.find_window_by_handle(win, error),
+            winid = winid,
+            window_ptr = ffi.C.find_window_by_handle(winid, ffi_error),
             fold = {
                 close = fillchars.foldclose,
                 open = fillchars.foldopen,
                 sep = fillchars.foldsep,
+                width = 0,
             },
         }
-        callargs[win] = args
+        args_by_winid[winid] = args
     end
+
     args.lnum = vim.v.lnum
     args.relnum = vim.v.relnum
     args.virtnum = vim.v.virtnum
-    args.fold.width = ffi.C.compute_foldcolumn(args.wp, 0)
+    args.fold.width = ffi.C.compute_foldcolumn(args.window_ptr, 0)
+
     return args
 end
 
--- Return the string that will be displayed in foldcolumn
--- Reference luukvbaal/statuscol.nvim's builtin.foldfunc
+local statuscolumn_augroup = vim.api.nvim_create_augroup('rockyz.statuscolumn_reference', { clear = true })
+
+vim.api.nvim_create_autocmd('WinClosed', {
+    group = statuscolumn_augroup,
+    callback = function(event)
+        args_by_winid[tonumber(event.match)] = nil
+    end,
+})
+
+---Return the string that will be displayed in foldcolumn
+---Reference luukvbaal/statuscol.nvim's builtin.foldfunc
+---@param args rockyz.StatuscolumnArgs
+---@return string
 local function foldfunc(args)
     local width = args.fold.width
     if width == 0 then
         return ''
     end
-    local foldinfo = ffi.C.fold_info(args.wp, args.lnum)
-    -- local string = args.cursorline and args.relnum == 0 and '%#CursorLineFold#' or '%#FoldColumn#'
-    local level = foldinfo.level
+
+    local fold_info = ffi.C.fold_info(args.window_ptr, args.lnum)
+    local level = fold_info.level
     if level == 0 then
         return (' '):rep(width)
     end
-    local closed = foldinfo.lines > 0
-    local first_level = level - width - (closed and 1 or 0) + 1
-    if first_level < 1 then
-        first_level = 1
-    end
-    -- For each column, add a foldopen, foldclose, foldsep or padding char
-    local range = level < width and level or width
-    local string = ''
-    -- Highlight the foldopen icon and foldclose icon on the current line
-    local open = (args.relnum == 0 and '%#CursorLineFold#' or '%#FoldColumn#') .. args.fold.open .. '%*'
-    local close = (args.relnum == 0 and '%#CursorLineFold#' or '%#FoldColumn#') .. args.fold.close .. '%*'
-    local sep = '%#FoldColumn#' .. args.fold.sep .. '%*'
-    for col = 1, range do
+
+    local is_closed = fold_info.lines > 0
+    local first_level = math.max(level - width - (is_closed and 1 or 0) + 1, 1)
+    local column_count = math.min(level, width)
+    local parts = {}
+
+    local fold_highlight = args.relnum == 0 and '%#CursorLineFold#' or '%#FoldColumn#'
+    local open = fold_highlight .. args.fold.open .. '%*'
+    local close = fold_highlight .. args.fold.close .. '%*'
+    local separator = '%#FoldColumn#' .. args.fold.sep .. '%*'
+
+    for column = 1, column_count do
         if args.virtnum ~= 0 then
-            string = string .. sep
-        elseif closed and (col == level or col == width) then
-            string = string .. close
-        elseif foldinfo.start == args.lnum and first_level + col > foldinfo.llevel then
-            string = string .. open
+            parts[#parts + 1] = separator
+        elseif is_closed and (column == level or column == width) then
+            parts[#parts + 1] = close
+        elseif fold_info.start == args.lnum and first_level + column > fold_info.llevel then
+            parts[#parts + 1] = open
         else
-            string = string .. sep
+            parts[#parts + 1] = separator
         end
     end
-    if range < width then
-        string = string .. (' '):rep(width - range)
+
+    if column_count < width then
+        parts[#parts + 1] = (' '):rep(width - column_count)
     end
-    return string
+
+    return table.concat(parts)
 end
 
--- Return line number
+---Return line number
+---@param args rockyz.StatuscolumnArgs
+---@return string
 local function lnumfunc(args)
-    if args.virtnum ~= 0 then
-        return '%='
-    end
-    return '%l'
+    return args.virtnum ~= 0 and '%=' or '%l'
 end
 
+---@return string
 function _G.statuscolumn()
-    local args = get_args()
-    local fold = foldfunc(args)
-    local lnum = lnumfunc(args)
-    return lnum .. '%s' .. fold .. ' '
+    local args = get_statuscolumn_args()
+    return lnumfunc(args) .. '%s' .. foldfunc(args) .. ' '
 end
 
 -- statuscolumn is local to window, so here both "%{% ... %}" and "%! ... " work (as for their

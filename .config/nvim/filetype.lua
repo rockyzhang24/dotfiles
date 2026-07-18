77c3d52ecb2e5c5620def45c6e938f557c722497
Rocky Zhang <yzhang.rocky@gmail.com>
2026-07-18 01:15:46 -0700

    refactor(nvim/filetype): clarify bigfile detection

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,25 +1,33 @@
 vim.filetype.add({
     pattern = {
-        -- Borrowed from folke/snacks.nvim. Set filetype to bigfile in order to disable some
-        -- features due to performance issues.
+        ---Detect large files early and assign the `bigfile` filetype to disable expensive features.
+        ---Based on the large-file detection in folke/snacks.nvim.
+        ---@param path? string
+        ---@param bufnr? integer
+        ---@return string?
         ['.*'] = function(path, bufnr)
             if not path or not bufnr or vim.bo[bufnr].filetype == 'bigfile' then
                 return
             end
+
             if path ~= vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr)) then
                 return
             end
-            local size = vim.fn.getfsize(path)
-            if size <= 0 then
+
+            local file_size = vim.fn.getfsize(path)
+            if file_size <= 0 then
                 return
             end
-            if size > vim.g.bigfile_size_threshold then
+
+            if file_size > vim.g.bigfile_size_threshold then
                 return 'bigfile'
             end
-            local lines = vim.api.nvim_buf_line_count(bufnr)
-            -- (size - lines) / lines: This gives the average length (i.e., average bytes) of the
-            -- content per line, excluding the newline character
-            return (size - lines) / lines > vim.g.bigfile_line_length_threshold and 'bigfile' or nil
+
+            local line_count = vim.api.nvim_buf_line_count(bufnr)
+
+            -- (file_size - line_count) / line_count: This gives the average length (i.e., average
+            -- bytes) of the content per line, excluding the newline character
+            return (file_size - line_count) / line_count > vim.g.bigfile_line_length_threshold and 'bigfile' or nil
         end,
     },
 })
